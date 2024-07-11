797184b9098091b048db5a632f373465e9b1e4be
Priscy Antonio Reales Mozo <realespriscy@hotmail.com>
2024-07-11 10:49:30 -0500

    First commit

---
+++ src/products/products.controller.ts
@@ -0,0 +1,73 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { CreateProductDto, UpdateProductDto } from './dto';
+import { PaginationDto } from 'src/common';
+
+@Controller('products')
+export class ProductsController {
+  constructor() {}
+
+  @Post()
+  createProduct(@Body() createProductDto: CreateProductDto) {
+    return this.client.send(
+      { cmd: 'create_product' },
+      createProductDto,
+    );
+  }
+
+  @Get()
+  findAllProducts(@Query() paginationDto: PaginationDto) {
+    return this.client.send(
+      { cmd: 'find_all_products' },
+      paginationDto,
+    );
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.client.send({ cmd: 'find_one_product' }, { id }).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
+
+    // try {
+
+    //   const product = await firstValueFrom(
+    //     this.productsClient.send({ cmd: 'find_one_product' },{ id })
+    //   );
+    //   return product;
+
+    // } catch (error) {
+    //   throw new RpcException(error);
+    // }
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') id: string) {
+    return this.client.send({ cmd: 'delete_product' }, { id }).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
+  }
+
+  @Patch(':id')
+  patchProduct(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    return this.client
+      .send(
+        { cmd: 'update_product' },
+        {
+          id,
+          ...updateProductDto,
+        },
+      )
+      .pipe(
+        catchError((err) => {
+          throw new RpcException(err);
+        }),
+      );
+  }
+}
